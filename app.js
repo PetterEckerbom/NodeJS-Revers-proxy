0b5411190a64d14e36f6355c210b5f16fc469ddb
PetterEckerbom <Petter.eckerbom@gmail.com>
2018-03-29 00:31:21 +0200

    Commented the code

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
-const projectsbp = 'http://localhost:3000',
+
+//Below are all the paths and ports to the diffrent servers that the calls should be rerouted to
+var projectsbp = 'http://localhost:3000',
     utomhusservice = 'http://localhost:4000',
     vergetracker = 'http://localhost:5000';
 	ninja = 'http://localhost:7000';
- 
+
+//Here all types of calls (GET , POST etc)	get handled
 app.all("/*", function(req, res) {
 	var host = req.get('host');
+	//The below if and ifElse statements check if the host/domain include the name of the specific server.
     if(host.indexOf("projectsbp") != -1){
+		//If the hostname includes the name it should, the call gets rerouted to the server path specified above
 		apiProxy.web(req, res, {target: projectsbp});
 	}else if(host.indexOf("utomhusservice") != -1){
 		apiProxy.web(req, res, {target: utomhusservice});
@@ -18,6 +23,7 @@ app.all("/*", function(req, res) {
 	}else if(host.indexOf("ninja") != -1){
 		apiProxy.web(req, res, {target: ninja});
 	}
+	//This can be scaled to include more servers by simply adding another ifElse here.
 });
 
 app.listen(80);
