3735600dcd358c83a8e5220d57a96b4ccb70b089
Petter Eckerbom <31358711+PetterEckerbom@users.noreply.github.com>
2018-04-25 10:51:24 +0200

    Cleaned up code
    Removed the awful looking ifelse statements for a cleaner looking and more scalable array solution

--- app.js
+++ app.js
@@ -4,27 +4,24 @@ const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
 
 //Below are all the paths and ports to the diffrent servers that the calls should be rerouted to
-var projectsbp = 'http://localhost:3000',
-    utomhusservice = 'http://localhost:4000',
-    vergetracker = 'http://localhost:5000';
-	ninja = 'http://localhost:7000';
+var locations= {
+    projectsbp: 'http://localhost:3000',
+    utomhusservice: 'http://localhost:4000',
+    vergetracker: 'http://localhost:5000',
+	  ninja : 'http://localhost:7000'
+  }
 
-//Here all types of calls (GET , POST etc)	get handled
-app.all("/*", function(req, res) {
-	var host = req.get('host');
-	//The below if and ifElse statements check if the host/domain include the name of the specific server.
-    if(host.indexOf("projectsbp") != -1){
-		//If the hostname includes the name it should, the call gets rerouted to the server path specified above
-		apiProxy.web(req, res, {target: projectsbp});
-	}else if(host.indexOf("utomhusservice") != -1){
-		apiProxy.web(req, res, {target: utomhusservice});
-	}else if(host.indexOf("vergetracker") != -1){
-		apiProxy.web(req, res, {target: vergetracker});
-	}else if(host.indexOf("ninja") != -1){
-		apiProxy.web(req, res, {target: ninja});
-	}
-	//This can be scaled to include more servers by simply adding another ifElse here.
-});
+  //Here all types of calls (GET , POST etc)	get handled
+  app.all("/*", function(req, res) {
+  	var host = req.get('host');
+    //Goes through all available hosts
+    for(key in locations){
+      //if host corresponds with a defining part of the name, the call gets forwarded to that server
+      if(host.indexOf(key) != -1){
+        apiProxy.web(req, res, {target: locations[key]});
+      }
+    }
+  });
 
 app.listen(80);
 console.log("Reverse proxy now running, Port 80")
